bd9a73daa7787a8a8a0d6009470e94d3eda74660
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-10 12:49:27 -0800

    add some comments to remote.sh script

--- scripts/remote.sh
+++ scripts/remote.sh
@@ -3,9 +3,13 @@
 user=$1
 home=/home/${user}
 
+# create new user with root priviliges
 useradd -d ${home} -s /bin/bash -m ${user}
 adduser ${user} sudo
 
+# copy ssh key for root user to admin
+# root ssh key is created automatically by DigitalOcean
+# when a new droplet is created
 ssh=${home}/.ssh
 keys=.ssh/authorized_keys
 mkdir ${ssh}
@@ -14,15 +18,21 @@ cp ${keys} ${home}/${keys}
 chmod 600 ${home}/${keys}
 chown -R ${user}:${user} ${ssh}
 
+# install puppet
 apt-get update
 apt-get install -y puppet
 
+# install required puppet modules
 puppet module install puppetlabs-java
 puppet module install amosjwood-neo4j
 
+# copy local modules into installation
 cp -r /tmp/puppet/modules/* /etc/puppet/modules
+
+# apply puppet configuration
 puppet apply /tmp/puppet/setup.pp
 
+# disable password logins
 sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
 echo "export EDITOR=/usr/bin/vim" >> /etc/environment
 
