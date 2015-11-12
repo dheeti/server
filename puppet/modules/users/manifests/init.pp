a920dda827915831b555b0d83b8bc30c100c729b
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-11 16:34:42 -0800

    clean up users puppet module

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -5,22 +5,12 @@ class users {
     }
 
     user { 'node-user':
-        require => Group['www-data'],
-        ensure => present,
-        groups => ['www-data'],
-        managehome => true,
-        shell => "/bin/bash",
+        require     => Group['www-data'],
+        ensure      => present,
+        groups      => ['www-data'],
+        managehome  => true,
+        shell       => "/bin/bash",
     }
 
-    user { 'jeff':
-        ensure => present,
-        managehome => true,
-        shell => "/bin/bash",
-    }
-
-    ssh_authorized_key { 'jeff_ssh':
-        user => "jeff",
-        type => "rsa",
-        key => "puppet:///modules/users/jeff.pub",
-    } 
 }
+
