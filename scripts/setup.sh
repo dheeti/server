58409fd441cff8a66a141444ade4a78066f8dd1c
Jeffrey Meyers <meyers2@pdx.edu>
2015-11-24 10:52:28 -0800

    remove neo4j stuff from bash provision script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-puppet_neo4j=https://github.com/meyersj/puppet-neo4j.git
-export NEO4J_PASSWORD=neo
-
 # install puppet
 apt-get update
 apt-get install -y git puppet
 
 # install required puppet modules
-puppet module install puppetlabs-java
 puppet module install puppetlabs-vcsrepo
-puppet module install puppetlabs-concat
-git clone -b develop $puppet_neo4j /etc/puppet/modules/neo4j
 
 # copy local modules into puppet modules
 cp -r /vagrant/puppet/modules/* /etc/puppet/modules
@@ -33,11 +27,6 @@ echo "export EDITOR=/usr/bin/vim" >> /etc/environment
 
 # ubuntu node uses `nodejs` executable while churchill build uses `node` executable
 ln -s `which nodejs` /usr/bin/node
-
-# set default password for neo4j user and build sample graph using abortion issue
-neoauth neo4j neo4j $NEO4J_PASSWORD
-service neo4j restart
-/vagrant/scripts/build_sample_graph.sh
 
 service ssh restart
 service churchill-node start
